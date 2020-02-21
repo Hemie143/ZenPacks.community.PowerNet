1d0a0068afca2023b4b8ff1d14423660b9f87f2c
Laurent Hemeryck <hemie143@gmail.com>
2020-02-21 10:09:52 +0100

    Graph sequence

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PowerNet"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 AUTHOR = "Laurent Hemeryck"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
